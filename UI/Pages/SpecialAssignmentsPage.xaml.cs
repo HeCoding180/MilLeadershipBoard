460fd01b4d81868da21051c60ba2de57b6f2165c
HeCoding180 MS Surface <HeCoding180@outlook.com>
2026-08-07 15:03:02 +0200

    Updated text and created a better general layout.

--- UI/Pages/SpecialAssignmentsPage.xaml.cs
+++ UI/Pages/SpecialAssignmentsPage.xaml.cs
@@ -21,9 +21,9 @@ namespace MilLeadershipBoard.UI.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SpecialCommandsPage : Page
+    public sealed partial class SpecialAssignmentsPage : Page
     {
-        public SpecialCommandsPage()
+        public SpecialAssignmentsPage()
         {
             InitializeComponent();
         }
